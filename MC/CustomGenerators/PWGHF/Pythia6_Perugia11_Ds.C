f4920fc6363f5228681cd2c6fcebd777d8492282
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2018-06-12 12:14:34 +0200

    fix vestigial array index

--- MC/CustomGenerators/PWGHF/Pythia6_Perugia11_Ds.C
+++ MC/CustomGenerators/PWGHF/Pythia6_Perugia11_Ds.C
@@ -2,7 +2,7 @@ AliGenerator *GeneratorCustom(TString opt = "")
 
 { // Generator for dedicated Ds production in pp for MVA training
 
-  Decay_t decay = AliDecayer::kPythia6HeavyDecay_Hadrons;
+  Decay_t decay = kPythia6HeavyDecay_Hadrons;
 
 //  AliGenCocktail *ctl = GeneratorCocktail("Perugia2011_HF");
   AliGenPythia* pyth = GeneratorPythia6(kPythia6Tune_Perugia2011);
@@ -10,7 +10,7 @@ AliGenerator *GeneratorCustom(TString opt = "")
   pyth->SetTriggerParticle(431,999,999,-1,1000); //Ds, etamin, etamax, ptmin, ptmax
   pyth->SetTriggerY(1.0);
   pyth->SetHeavyQuarkYRange(-1.5,1.5);
-  pyth->SetForceDecay(decay[idecay]);
+  pyth->SetForceDecay(decay);
   pyth->SetPtHard(pthardminConfig,pthardmaxConfig);
   return pyth;
 
